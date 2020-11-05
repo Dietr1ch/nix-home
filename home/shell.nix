96c6087c3a21cf35999a0dfaebf996660ae6a06b
Dietrich Daroch <dietr1ch@acm.org>
2020-11-04 21:38:44 -0800

    Shell: Update starship prompt

--- home/shell.nix
+++ home/shell.nix
@@ -195,13 +195,6 @@ with import <nixpkgs> {};
         add_newline = true;
         character.symbol = "❯\n";
 
-        cmd_duration.min_time = 500;
-        cmd_duration.show_milliseconds = true;
-        cmd_duration.prefix = "⏳";
-
-        time.format = "%F %a 🕙 %T ";
-        time.disabled = false;
-
         crystal.disabled = false;
         dotnet.disabled = false;
         elixir.disabled = false;
@@ -215,42 +208,69 @@ with import <nixpkgs> {};
         aws.disabled = false;
         conda.disabled = false;
 
-        prompt_order = [
-          "jobs"
-          "battery"
-          "time"
-          "cmd_duration"
+        directory = {
+          fish_style_pwd_dir_length = 1;
+          truncation_length = 4;
+        };
+        git_branch = {
+          format = "[$symbol $branch]($style) ";
+          symbol = "";
+        };
 
-          "line_break"
+        cmd_duration = {
+          format = " ⏳[$duration]($style)";
+          show_milliseconds = true;
 
-          "username"
-          "hostname"
+          min_time = 500;  # 500ms
+          # Notifications
+          show_notifications = true;
+          min_time_to_notify = 5000;  # 5s
+        };
+        time = {
+          disabled = false;
+          format = "[$time]($style)";
+          time_format = "%F %a 🕙 %T ";
+        };
 
-          "directory"
+
+        # 2020-11-03 Tue 🕙 22:21:33  ⏳23s99ms
+        # ~ on  Dietr1ch [✘!] ❯
+        # echo "aoeu"
+        format = builtins.concatStringsSep "" [
+          "$jobs"
+          "$battery"
+          "$time"
+          "$cmd_duration"
+
+          "$line_break"
+
+          "$username"
+          "$hostname"
+
+          "$directory"
 
           # "kubernetes"
-          "git_branch"
-          "git_commit"
-          "git_state"
-          "git_status"
-          "hg_branch"
-          # "docker_context"
-          "package"
+          "$git_branch"
+          "$git_commit"
+          "$git_state"
+          "$git_status"
+          "$hg_branch"
+          # "$docker_context"
+          "$package"
 
-          "golang"
-          "haskell"
-          "python"
-          "ruby"
-          "rust"
+          "$golang"
+          "$haskell"
+          "$python"
+          "$ruby"
+          "$rust"
 
-          "terraform"
-          "nix_shell"
-          "memory_usage"
-          # "env_var"
+          "$terraform"
+          "$nix_shell"
+          "$memory_usage"
+          # "$env_var"
 
-          "character"
-          "line_break"
-          # "custom"
+          "$character"
+          "\n\n"
         ];
       };
     };
