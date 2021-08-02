72105c6f3d988f0a764dcb0018d9b787da3a7073
Dietrich Daroch <Dietrich.Daroch@gmail.com>
2021-08-01 22:37:43 -0700

    Config: Switch to unstable OBS

--- config.nix
+++ config.nix
@@ -1,3 +1,6 @@
+let
+  unstableTarball = fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz";
+in
 {
   allowUnfree = true;
 
@@ -8,5 +11,12 @@
   firefox = {
     enablePlasmaBrowserIntegration = true;
   };
-}
 
+  packageOverrides = pkgs: rec {
+    unstable = import unstableTarball { };
+    obs = pkgs.buildEnv {
+      name = "obs settings unstable";
+      paths = [ unstable.obs-studio ];
+    };
+  };
+}
