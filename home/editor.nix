855303d6efc47d9e1e4fa40d4e8da58b85a090c0
Dietrich Daroch <Dietrich.Daroch@gmail.com>
2022-05-22 23:02:54 -0700

    Emacs: Stop using emacs-overlay
    Emacs 28 is out, which is modern enough.

--- home/editor.nix
+++ home/editor.nix
@@ -5,8 +5,6 @@
     emacs = {
       enable = true;
 
-      package = pkgs.emacsPgtkNativeComp;
-      # package = pkgs.emacsUnstable;
       extraPackages = epkgs: with epkgs; [
         # exwm
         emacsql-sqlite
@@ -51,13 +49,4 @@
       };
     };
   };
-
-  nixpkgs.overlays = [
-    # Emacs
-    # https://gist.github.com/mjlbach/179cf58e1b6f5afcb9a99d4aaf54f549
-    (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-      sha256 = "0phc2qwqgfa8rp9bcn36d479l3hl77x0kg2398vdjj730h43wcql";
-    }))
-  ];
 }
